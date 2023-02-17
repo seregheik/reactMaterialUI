c36c46f248a617b408b285132bfd3e7552d265c1
El-Benson <122732891+El-Benson@users.noreply.github.com>
2023-02-17 12:32:56 +0100

    typography

---
+++ src/components/MuiTypography.jsx
@@ -0,0 +1,26 @@
+import { Typography } from "@mui/material";
+
+const MuiTypography = () => {
+    return (<>
+        <Typography variant='h1'>h1 heading</Typography>
+        <Typography variant='h2'>h2 heading</Typography>
+        <Typography variant='h3'>h3 heading</Typography>
+        <Typography variant='h4'>h4 heading</Typography>
+        <Typography variant='h5'>h5 heading</Typography>
+        <Typography variant='h6'>h6 heading</Typography>
+
+        <Typography variant='subtitle1'>sub title 1</Typography>
+        <Typography variant='subtitle2'>sub title 2</Typography>
+
+        <Typography variant='body1'>Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+            Numquam voluptatem molestias, praesentium earum laborum et error in iste corrupti laudantium
+            neque nulla temporibus voluptate, totam omnis nisi repellendus a. Libero!</Typography>
+
+        <Typography variant='body2'>Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+            Explicabo voluptatibus voluptates unde quaerat dolore, ducimus neque, quidem at optio recusandae
+            ipsa! Dignissimos fuga corporis voluptas consequatur, veritatis architecto quasi fugiat?</Typography>
+    </>
+    )
+}
+
+export default MuiTypography
